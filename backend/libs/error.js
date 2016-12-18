fbd241857b7e1c22a1a7fc12581ad2eec87a054d
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-12-18 21:01:00 +0500

    Added support custom errors

---
+++ backend/libs/error.js
@@ -0,0 +1,33 @@
+'use strict';
+/** Requires */
+import ExtendableError from 'es6-error';
+
+export class HttpError extends ExtendableError {
+  constructor(message, status = 400) {
+    super(message);
+
+    this.type = 'RequestError';
+    this.status = status;
+  }
+}
+
+export class AuthError extends HttpError {
+  constructor(message, status = 400) {
+    super(message);
+
+    this.type = 'AuthError';
+    this.status = status;
+  }
+}
+
+export class AuthLocalError extends AuthError {
+  constructor(message, status, email, password) {
+    super(message, status);
+
+    this.email = email;
+
+    Object.defineProperty(this, 'password', {
+      value: password
+    });
+  }
+}
